5bb4e0d63f321e6aaed7c430086b9bea2e2f1001
hearecho <1540302560@qq.com>
2020-10-19 19:14:12 +0800

    增加直播相关操作

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ![deliveryCoin](https://github.com/hearecho/taskScript/workflows/deliveryCoin/badge.svg)
 # taskScript
 使用github action执行的各种定时脚本
+- [x] b站定期从关注列表中选择视频投币，每天投五个币，随机视频
+- [x] b站每天银瓜子转换硬币
+- [x] b站每天清空直播包裹将过期（一天以内过期）礼物
+ 
---
+++ live.py
@@ -0,0 +1,69 @@
+"""
+直播相关的api
+"""
+import os
+import time
+
+from utils import init
+
+
+def getRoomInfo(s, room_id):
+    """
+    获取房间信息
+    :param s: session
+    :param room_id: 显示的房间ID
+    :return: data字典
+    """
+    url = "https://api.live.bilibili.com/xlive/web-room" \
+          "/v1/index/getRoomPlayInfo?room_id={}".format(room_id)
+    r = s.get(url)
+    return r.json()["data"]
+
+
+def getDanMuCong(s, room_id):
+    """
+    获取弹幕服务器信息
+    :param s: session
+    :param room_id: 显示的房间ID
+    :return: data字典
+    """
+    url = "https://api.live.bilibili.com/room/v1/Danmu/getConf?room_id={}".format(room_id)
+    r = s.get(url).json()
+    return r["data"]
+
+
+def getBagList(s, room_id):
+    r = s.get("https://api.live.bilibili.com/xlive/web-room/v1/gift/bag_list?room_id={}".format(room_id))
+    return r.json()["data"]["list"]
+
+
+def sendWillExpireGift(s, room_info, bage_info):
+    # 将包裹中时间快到期的包裹送出
+    count = 0
+    t = int(time.time())
+    daySecond = 24 * 60 * 60
+    data = {"uid": os.environ["DEDEUSERID"],
+            "gift_id": 1,
+            "ruid": room_info["uid"],
+            "gift_num": 1,
+            "bag_id": 221873830,
+            "biz_id": room_info["room_id"],
+            "csrf_token": os.environ["BILI_JCT"]
+            }
+    # 判断过期
+    for g in bage_info:
+        if (g["expire_at"] - t) / daySecond < 1:
+            # 送出礼物
+            data["gift_id"] = g["gift_id"]
+            data["gift_num"] = g["gitt_num"]
+            data["bag_id"] = g["bag_id"]
+            r = s.post("https://api.live.bilibili.com/gift/v2/live/bag_send", data=data)
+            print(r.json())
+    print("送出将过期礼物{}".format(count))
+
+
+if __name__ == '__main__':
+    s, api = init.init()
+    room_info = getRoomInfo(s, 102)
+    bag_info = getBagList(s, 102)
+    sendWillExpireGift(s, room_info, bag_info)
